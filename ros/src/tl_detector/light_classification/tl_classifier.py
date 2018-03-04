2a2f048a872728c1e9d21ff0c24d5eba56fa663d
Anton Stavitskiy <anton.stavitskiy@gmail.com>
2018-03-04 20:24:00 +0300

    modified tl_detector

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,6 @@
 from styx_msgs.msg import TrafficLight
+import cv2
+import numpy as np
 
 class TLClassifier(object):
     def __init__(self):
@@ -16,4 +18,34 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        #return TrafficLight.UNKNOWN
+        
+        output = image.copy()
+        red = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+
+        lower_red = np.array([0,50,50])
+        upper_red = np.array([10,255,255])
+        red1 = cv2.inRange(red, lower_red, upper_red)
+
+
+        lower_red = np.array([170,50,50])
+        upper_red = np.array([180,255,255])
+        red2 = cv2.inRange(red, lower_red, upper_red)
+
+        converted_img = cv2.addWeighted(red1, 1.0, red2, 1.0, 0.0)
+
+        blur_img = cv2.GaussianBlur(converted_img,(15,15),0)
+
+        circles = cv2.HoughCircles(blur_img,cv2.HOUGH_GRADIENT,0.5,41, param1=70,param2=30,minRadius=5,maxRadius=150)
+
+        if circles is not None:
+            result = TrafficLight.RED
+            #cv2.imwrite("red.png", blur_img)
+        else:
+            result = TrafficLight.UNKNOWN
+
+        #cv2.imwrite("image.png", image)
+        #cv2.imwrite("result.png", blur_img)
+
+        return result, output
