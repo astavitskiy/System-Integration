6adc600450234342a48dc91886a2d27112e01d9a
Anton Stavitskiy <anton.stavitskiy@gmail.com>
2018-03-05 01:30:52 +0300

    added deceleration

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -24,7 +24,9 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
-
+TARGET_SPEED = 10   # [MPH]
+MAX_DECEL = 0.7
+STOP_DIST = 10.0
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -53,19 +55,70 @@ class WaypointUpdater(object):
         pass
         
     def publish_final_wp(self):
-        self.nxtWayPoint = self.next_way_point()
-        if self.nxtWayPoint != -1:
-            print(self.nxtWayPoint)
-            waypoints = self.baseWayPoints.waypoints
-            self.final_waypoints.header.seq = self.ID
-            ++self.ID
-            self.final_waypoints.header.stamp = rospy.get_rostime()
-            if (abs(self.nxtWayPoint - len(waypoints)) > LOOKAHEAD_WPS):
-                self.final_waypoints.waypoints = waypoints[self.nxtWayPoint: 
-                                                    self.nxtWayPoint+LOOKAHEAD_WPS]
+
+        #if self.vehiclePose.pose is not None:
+
+        nxtWayPoint = self.next_way_point()
+        lookahead_waypoints = self.baseWayPoints.waypoints[nxtWayPoint:nxtWayPoint+LOOKAHEAD_WPS]
+
+        if self.traffic_light_waypoint is None or self.traffic_light_waypoint < 0:
+            # No RED lights, setting the velocity
+            for i in range(len(lookahead_waypoints) - 1):
+                # convert [MPH] to meters per sec
+                self.set_waypoint_velocity(lookahead_waypoints, i, (TARGET_SPEED * 1609.34) / (60 * 60))
+
+        else:
+            tl_lookahead_index = max(0, self.traffic_light_waypoint - nxtWayPoint)
+            lookahead_waypoints = self.decelerate(lookahead_waypoints, tl_lookahead_index)
+
+        lane = Lane()
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time(0)
+        lane.waypoints = lookahead_waypoints  
+        self.final_waypoints_pub.publish(lane)
+
+    # def publish_final_wp(self):
+    # self.nxtWayPoint = self.next_way_point()
+    # if self.nxtWayPoint != -1:
+    #     print(self.nxtWayPoint)
+    #     waypoints = self.baseWayPoints.waypoints
+    #     self.final_waypoints.header.seq = self.ID
+    #     ++self.ID
+    #     self.final_waypoints.header.stamp = rospy.get_rostime()
+    #     if (abs(self.nxtWayPoint - len(waypoints)) > LOOKAHEAD_WPS):
+    #         self.final_waypoints.waypoints = waypoints[self.nxtWayPoint: 
+    #                                             self.nxtWayPoint+LOOKAHEAD_WPS]
+    #     else:
+    #         self.final_waypoints.waypoints = waypoints[self.nxtWayPoint:]
+    #     self.final_waypoints_pub.publish(self.final_waypoints)
+
+    def decelerate(self, waypoints, redlight_index):
+        # decelerate the vehicle
+        if len(waypoints) < 1:
+            return []
+
+        first = waypoints[0]
+        last = waypoints[redlight_index]
+
+        last.twist.twist.linear.x = 0.
+        total_dist = self.distance(first.pose.pose.position, last.pose.pose.position)
+        start_vel = first.twist.twist.linear.x
+        # start from the waypoint before last and go backwards
+        for index, wp in enumerate(waypoints):
+            if index > redlight_index:
+                vel = 0
             else:
-                self.final_waypoints.waypoints = waypoints[self.nxtWayPoint:]
-            self.final_waypoints_pub.publish(self.final_waypoints)
+                dist = self.distance(wp.pose.pose.position, last.pose.pose.position)
+                dist = max(0, dist - STOP_DIST)                
+                vel  = math.sqrt(2 * MAX_DECEL * dist) 
+                if vel < 1.:
+                    vel = 0.
+            wp.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
+
+        return waypoints 
+
+
+ 
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
@@ -88,6 +141,11 @@ class WaypointUpdater(object):
     def distance_bw(self, w1, w2):
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
         return dl(w1.pose.position, w2.pose.pose.position)
+
+    def distance(self, p1, p2):
+        x, y, z = p1.x - p2.x, p1.y - p2.y, p1.z - p2.z
+        dist = math.sqrt(x*x + y*y + z*z)
+        return dist
 
     def closest_way_point(self):
         waypointslist = self.baseWayPoints.waypoints
@@ -121,8 +179,7 @@ class WaypointUpdater(object):
             if(closestWayPoint == len(self.baseWayPoints.waypoints)):
                 closestWayPoint = 0
    
-        return closestWayPoint
-            
+        return closestWayPoint     
     
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
@@ -130,13 +187,13 @@ class WaypointUpdater(object):
     def set_waypoint_velocity(self, waypoints, waypoint, velocity):
         waypoints[waypoint].twist.twist.linear.x = velocity
 
-    def distance(self, waypoints, wp1, wp2):
-        dist = 0
-        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-        for i in range(wp1, wp2+1):
-            dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
-            wp1 = i
-        return dist
+    # def distance(self, waypoints, wp1, wp2):
+    #     dist = 0
+    #     dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+    #     for i in range(wp1, wp2+1):
+    #         dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
+    #         wp1 = i
+    #     return dist
 
 
 if __name__ == '__main__':
